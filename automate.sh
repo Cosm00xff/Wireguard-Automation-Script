f59fe7c3a9401558595a8d8aad73cb9fe2c15845
Cosmo <124483350+Cosm00xff@users.noreply.github.com>
2023-02-05 23:28:09 -0600

    Add files via upload

--- automate.sh
+++ automate.sh
@@ -2,7 +2,6 @@
 
 # Preprocessor Variables
 export SERV_IP_ADDR="77.73.69.183"
-export AUTO_CONFIG_HOME="/root/Automate_Func/"
 
 function menu {
 echo "
@@ -47,37 +46,39 @@ echo "
 }
 
 function createInterface {
+    mkdir -p /etc/wireguard/
     read -p "Please enter a name for the server interface: " serverInterfaceName
     wg genkey | tee /etc/wireguard/${serverInterfaceName}_private.key | wg pubkey | tee /etc/wireguard/${serverInterfaceName}_public.key
 
     echo "[Interface]
-Address = 10.10.10.1/24
-SaveConfig = true
-PrivateKey = $(cat /etc/wireguard/${serverInterfaceName}_private.key)
-ListenPort = 51820
-
-[Peer]
-PublicKey = $(cat /etc/wireguard/${serverInterfaceName}_private.key)
-AllowedIPs = 10.10.10.2/32" > /etc/wireguard/${serverInterfaceName}.conf
+          Address = 10.10.10.1/24
+          SaveConfig = true
+          PrivateKey = $(cat /etc/wireguard/${serverInterfaceName}_private.key)
+          ListenPort = 51820" > /etc/wireguard/${serverInterfaceName}.conf
 
     chmod 600 /etc/wireguard/ -R
 }
 
 # function manageUsers { echo "HELLO" }
 function createUser { 
+    mkdir -p /etc/wireguard/
     read -p "Please enter a name for new client configuration: " clientName
     wg genkey | tee /etc/wireguard/${clientName}_private.key | wg pubkey | tee /etc/wireguard/${clientName}_public.key
 
-echo "[Interface]
-Address = 10.10.10.2/24
-DNS = 10.10.10.1
-PrivateKey = $(cat /etc/wireguard/${clientName}_private.key)
+    echo "[Interface]
+          Address = 10.10.10.2/24
+          DNS = 10.10.10.1
+          PrivateKey = $(cat /etc/wireguard/${clientName}_private.key)
 
-[Peer]
-PublicKey =  $(cat /etc/wireguard/${clientName}_public.key)
-AllowedIPs = 0.0.0.0/0
-Endpoint = $SERV_IP_ADDR:51820
-PersistentKeepalive = 25" > /etc/wireguard/${clientName}.conf
+          [Peer]
+          PublicKey =  $(cat /etc/wireguard/wg0_public.key)
+          AllowedIPs = 0.0.0.0/0
+          Endpoint = $SERV_IP_ADDR:51820
+          PersistentKeepalive = 25" > /etc/wireguard/${clientName}.conf
+
+    echo "[Peer]
+          PublicKey = $(cat /etc/wireguard/${clientName}_public.key)
+          AllowedIPs = 10.0.0.2/32" >> /etc/wireguard/wg0.conf
 
     chmod 600 /etc/wireguard/ -R
 }
@@ -89,34 +90,28 @@ function deleteUser {
 # function checkServices { echo "HELLO" }
 function installation {
     # Install required packages
-    yum update && yum upgrade -y
-    yum install redhat-lsb-core -y
-    yum install epel-release -y
-    yum install yum-plugin-elrepo -y
-    yum install kmod-wireguard -y
-    yum install wireguard-tools -y
+    yum install yum-utils epel-release -y
+    yum-config-manager --setopt=centosplus.includepkgs=kernel-plus --enablerepo=centosplus --save
+    sed -e 's/^DEFAULTKERNEL=kernel$/DEFAULTKERNEL=kernel-plus/' -i /etc/sysconfig/kernel
+    yum install kernel-plus wireguard-tools -y
     yum install bind -y
-    yum install nano -y
-
-    # PKI Configuration
-    mkdir -p /etc/wireguard/
-    chmod 600 /etc/wireguard/ -R
+    # REBOOT NEEDED HERE IF MODULE NOT LOADED || VERIFY THROUGH MODPROBE
 
     # Configure Wireguard Server Interface
     createInterface
 
     # Enable IP Forwarding
     echo "# sysctl settings are defined through files in
-# /usr/lib/sysctl.d/, /run/sysctl.d/, and /etc/sysctl.d/.
-#
-# Vendors settings live in /usr/lib/sysctl.d/.
-# To override a whole file, create a new file with the same in
-# /etc/sysctl.d/ and put new settings there. To override
-# only specific settings, add a file with a lexically later
-# name in /etc/sysctl.d/ and put new settings there.
-#
-# For more information, see sysctl.conf(5) and sysctl.d(5).
-net.ipv4.ip_forward = 1" > /etc/sysctl.conf
+          # /usr/lib/sysctl.d/, /run/sysctl.d/, and /etc/sysctl.d/.
+          #
+          # Vendors settings live in /usr/lib/sysctl.d/.
+          # To override a whole file, create a new file with the same in
+          # /etc/sysctl.d/ and put new settings there. To override
+          # only specific settings, add a file with a lexically later
+          # name in /etc/sysctl.d/ and put new settings there.
+          #
+          # For more information, see sysctl.conf(5) and sysctl.d(5).
+          net.ipv4.ip_forward = 1" > /etc/sysctl.conf
 
     sysctl -p
 
@@ -127,7 +122,6 @@ net.ipv4.ip_forward = 1" > /etc/sysctl.conf
     # Install a DNS Resolver
     systemctl start named
     systemctl enable named
-    cd $AUTO_CONFIG_HOME
     cp /root/Automate_Func/named.conf /etc/named.conf
     chmod 777 /etc/named.conf
     systemctl restart named
@@ -138,6 +132,8 @@ net.ipv4.ip_forward = 1" > /etc/sysctl.conf
     firewall-cmd --permanent --add-port=51820/udp
     systemctl reload firewalld
 
+    # Start Wireguard
+    wg-quick up /etc/wireguard/wg0.conf
     echo "Installation Complete, returning to menu!"
     echo ""
     menu
